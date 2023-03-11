33ff16aff434d487b45ea3eb15d6277dd213bb53
iagatha <iagathasouzaa@gmail.com>
2023-03-11 09:31:09 -0300

    vetor

---
+++ media_idade.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void){
+    int idade[3] = {0, 0, 0};
+    float media = 0.0;
+
+    printf("\n Informe a idade da primeira pessoa: ");
+    scanf("%d", &idade[0]);
+    printf("\n Informe a idade da segunda pessoa: ");
+    scanf("%d", &idade[1]);
+    printf("\n Informe a idade da terceira pessoa: ");
+    scanf("%d", &idade[2]);
+
+    media = (idade[0] + idade[1] + idade[2]) /3;
+    printf("\n Média das idades = %.2f", media);
+
+    return 0;
+}
