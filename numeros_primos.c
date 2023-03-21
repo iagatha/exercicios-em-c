9e1bd2312362608d337a6381742f1c0497146a3d
iagatha <iagathasouzaa@gmail.com>
2023-03-21 11:34:49 -0300

    função recursiva

---
+++ numeros_primos.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int ehPrimo(int n){
+    return ehPrimo(n, 2);
+}
+int ehPrimoAux(int n, int x){
+    if(x == n){
+        return 1;
+    }else if(n % x == 0){
+        return 0;
+    }else {
+        return ehPrimoAux(n, x + 1);
+    }
+}
+int main(){
+    int resultado = 0,n;
+    printf("\nDigite um numero inteiro positivo: ");
+    scanf("%d", &n);
+
+    resultado =ehPrimo(n);
+    printf("\nResultado do fatorial = %d", resultado);
+    return 0;
+}
