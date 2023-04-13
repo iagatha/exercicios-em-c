e787694c7f6affc5dedb5e6bee58a7e4199b1740
Iagatha de Souza <85373818+iagatha@users.noreply.github.com>
2023-04-12 21:15:18 -0300

    Update calculadora.c

--- calculadora.c
+++ calculadora.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 int main(void){
     float x=0,y=0, resultado=0;
-    int op=0;
+    int opcao=0;
     do{
         printf("\n t1 - Soma\n\t2 - subtracao\n\t3 - multiplicacao\nt4 - divisao");
-        scan("%d", &op);
+        scan("%d", &opcao);
         printf("\nDigite o primeiro numero: ");
         scanf("%f", &x);
         printf("\nDigite o segundo numero: ");
         scanf("%f", &y);
-        switch(op){
+        switch(opcao){
             case 1:
                 resultado = x+y;
                 break;
@@ -28,6 +28,6 @@ int main(void){
         }
         printf("\nt O resultado e: %0.2f", resultado);
         printf("\nDigite 1 para continuar: ");
-        scanf("%i", &op);
-    }while(op==1);
+        scanf("%i", &opcao);
+    }while(opcao==1);
 }
